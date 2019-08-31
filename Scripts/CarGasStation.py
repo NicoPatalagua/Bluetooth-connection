070fe4458ea28dcdc59f011ca21cbe0ddfd5c05e
Nico Patalagua <nicopatalagua@gmail.com>
2019-08-31 16:19:15 -0500

    Scripts

---
+++ Scripts/CarGasStation.py
@@ -0,0 +1,37 @@
+#Author: Nico Patalagua
+
+import bluetooth
+
+def receiveMessages():
+  server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+  
+  port = 1
+  server_sock.bind(("",port))
+  server_sock.listen(1)
+  
+  client_sock,address = server_sock.accept()
+  print "Accepted connection from " + str(address)
+  
+  data = client_sock.recv(1024)
+  print "received [%s]" % data
+  
+  client_sock.close()
+  server_sock.close()
+  
+def sendMessageTo(targetBluetoothMacAddress):
+  port = 1
+  sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+  sock.connect((targetBluetoothMacAddress, port))
+  sock.send("Hello service station, this car is owned by Nico Patalagua")
+  sock.close()
+  
+def lookUpNearbyBluetoothDevices():
+  print("OK")
+  nearby_devices = bluetooth.discover_devices()
+  for bdaddr in nearby_devices:
+    print str(bluetooth.lookup_name( bdaddr )) + " [" + str(bdaddr) + "]"
+    #sendMessageTo(bdaddr)
+  sendMessageTo("B8:27:EB:FC:72:30")
+    
+lookUpNearbyBluetoothDevices() 
+
